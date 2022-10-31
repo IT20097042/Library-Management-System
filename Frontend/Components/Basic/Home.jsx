b25c32063cf1a6d0f32252ddda616f08926331ec
IT20097042 <87925907+IT20097042@users.noreply.github.com>
2022-10-31 07:30:52 +0530

    style: add a image to home page

--- Frontend/Components/Basic/Home.jsx
+++ Frontend/Components/Basic/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
 
 function Home(props) {
-    return (
-        <>
-            <center> <h2> Library Application!!</h2>  </center>
-        </>
- 
-    );
+  return (
+    <>
+      <center>
+        {" "}
+        <h2> Library Application!!</h2>
+        <img
+          src={require("../static/Images/library.jpg")}
+          width="700"
+          height="430"
+        />
+      </center>
+    </>
+  );
 }
 
 export default Home;
