d6e1c0b130fd04bb220a7416322bc945ed29b826
IT20097042 <87925907+IT20097042@users.noreply.github.com>
2022-10-31 07:07:16 +0530

    style: change format of the nav bar

--- Frontend/Components/Basic/Navigation.jsx
+++ Frontend/Components/Basic/Navigation.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import {FaFileSignature} from 'react-icons/fa'
-import {ImDisplay} from 'react-icons/im'
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import {ImUser} from 'react-icons/im'
+import {GrDocumentUser} from 'react-icons/gr'
+import {MdOutlineMenuBook, MdLocalLibrary} from 'react-icons/md'
+import {BsBookFill} from 'react-icons/bs'
+
 
 
 
@@ -14,29 +15,29 @@ function Navigation(props) {
         <>
             <nav className='navbar navbar-expand-lg bg-light rounded'>
             <div className= 'container-fluid'>
-                                    <Link  to='/'> <h1 className="navbar-brand ">Library App </h1></Link>
+                                    <Link  to='/'> <h1 className="navbar-brand "><MdLocalLibrary color="blue"/>Library App </h1></Link>
                                 </div>
 
                                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                     <li className="nav-item">
                                         <Link to='/insertBook' className="nav-link">
-                                            <h3><FaFileSignature color="black"/> Insert Book </h3>
+                                            <h4><MdOutlineMenuBook color="black"/> Insert Book </h4>
                                         </Link>
                                     </li>
 
                                     <li className="nav-item">
                                         <Link to='/insertAuthor' className="nav-link">
-                                            <h3><FaFileSignature color="black"/> Insert Author </h3>
+                                            <h4><GrDocumentUser color="black"/> Insert Author </h4>
                                         </Link>
                                     </li>
                                     <li className="nav-item">
                                         <Link to='/viewBook' className="nav-link">
-                                            <h3><ImDisplay color="black"/> Books </h3>
+                                            <h4><BsBookFill color="black"/> Books </h4>
                                         </Link>
                                     </li>
                                     <li className="nav-item">
                                         <Link to='/viewAuthor' className="nav-link">
-                                            <h3><ImDisplay color="black"/> Authors </h3>
+                                            <h4><ImUser color="black"/> Authors </h4>
                                         </Link>
                                     </li>
                                 </ul>
